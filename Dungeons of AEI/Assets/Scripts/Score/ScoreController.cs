ba15c96db1ffdfbc8c731b119f421ce1fc800ce2
kamiszo821 <72297505+kamiszo821@users.noreply.github.com>
2023-05-30 12:43:21 +0200

    next level by door and remember coins amount

--- Dungeons of AEI/Assets/Scripts/Score/ScoreController.cs
+++ Dungeons of AEI/Assets/Scripts/Score/ScoreController.cs
@@ -6,13 +6,35 @@ using UnityEngine.UI;
 public class ScoreController : MonoBehaviour
 {
     private int score = 0;
-
+    private static ScoreController instance = null;
     [SerializeField]
     public Text scoreText;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            if (this != instance)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
 
     private void OnEnable()
     {
         CoinPick.picked += AddScore;
+    }
+
+    private void OnDisable()
+    {
+        CoinPick.picked -= AddScore;
     }
 
     public void AddScore()
